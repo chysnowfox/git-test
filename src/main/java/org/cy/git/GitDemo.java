0e8258f120b313035429799e448ad80157718e97
snowfox <snowfox@163.com>
2023-09-17 18:43:16 +0800

    modify on branch master code

--- src/main/java/org/cy/git/GitDemo.java
+++ src/main/java/org/cy/git/GitDemo.java
@@ -8,6 +8,7 @@ public class GitDemo {
         System.out.println("hello GitDemo first and x");
         System.out.println("hello GitDemo-2 first and x");
         System.out.println("hello GitDemo-2 first and yyy");
+        System.out.println("hello GitDemo-2 master modify");
 
     }
 }
