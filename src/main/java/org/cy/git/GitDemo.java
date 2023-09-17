d2696f7ac9e763aa64ff1c03d334191819380303
snowfox <snowfox@163.com>
2023-09-17 17:58:16 +0800

    frist cy code

--- src/main/java/org/cy/git/GitDemo.java
+++ src/main/java/org/cy/git/GitDemo.java
@@ -5,6 +5,7 @@ package org.cy.git;
  */
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("hello GitDemo");
+        System.out.println("hello GitDemo first and x");
+
     }
 }
