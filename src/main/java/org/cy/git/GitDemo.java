e6f94a470c980cb2391eb5cb57dd060d09504cdd
snowfox <snowfox@163.com>
2023-09-17 18:31:14 +0800

    frist hot-fix cy code

--- src/main/java/org/cy/git/GitDemo.java
+++ src/main/java/org/cy/git/GitDemo.java
@@ -7,6 +7,7 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello GitDemo first and x");
         System.out.println("hello GitDemo-2 first and x");
+        System.out.println("hello GitDemo-2 first and yyy");
 
     }
 }
